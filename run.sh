ecdaccf6b9bd2750b7c8a888861b7dd65102a049
papo1011 <francesco.papini.siena@gmail.com>
2025-12-26 00:22:55 +0100

    feat: add env config and model download logic

--- run.sh
+++ run.sh
@@ -1,13 +1,30 @@
 EXE_NAME="fast-detect-gpt"
-MODEL_NAME="Qwen2.5-Coder-7B-Instruct-Q3_K_M.gguf"
-INPUT_FILE="Qwen2.5-Coder-7B-Instruct.py"
-
-MODEL_DIR="models"
 BUILD_DIR="cmake-build-debug"
+MODEL_DIR="models"
 INPUT_DIR="inputs"
+
+ENV_FILE=".env"
+
+if [ ! -f "$ENV_FILE" ]; then
+    echo "'$ENV_FILE' not found. Please create it"
+    exit 1
+fi
+
+set -a
+source "$ENV_FILE"
+set +a
 
 EXECUTABLE="./$BUILD_DIR/$EXE_NAME"
 MODEL_PATH="./$MODEL_DIR/$MODEL_NAME"
 INPUT_PATH="./$INPUT_DIR/$INPUT_FILE"
+
+MODEL_BASE_URL="https://$HF_TOKEN@huggingface.co"
+MODEL_URL="$MODEL_BASE_URL/$MODEL_HF_PATH?download=true"
+
+if [ ! -f "$MODEL_PATH" ]; then
+    echo "Model not found on disk. Download..."
+    mkdir -p "$MODEL_DIR"
+    wget --show-progress -c "$MODEL_URL" -O "$MODEL_PATH"
+fi
 
 $EXECUTABLE "$MODEL_PATH" "$INPUT_PATH"
